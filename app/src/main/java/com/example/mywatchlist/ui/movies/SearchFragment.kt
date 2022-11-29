23d59bec9cbf319c15f8f282de93e4b1f7568438
jay <jaymaurya1256@gmail.com>
2022-11-29 13:52:09 +0530

    Implement search movie functionality

--- app/src/main/java/com/example/mywatchlist/ui/movies/SearchFragment.kt
+++ app/src/main/java/com/example/mywatchlist/ui/movies/SearchFragment.kt
@@ -1,17 +1,26 @@
 package com.example.mywatchlist.ui.movies
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.mywatchlist.R
 import com.example.mywatchlist.databinding.FragmentSearchBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
+
+private const val TAG = "SearchFragment"
 
 class SearchFragment : BottomSheetDialogFragment() {
     lateinit var binding : FragmentSearchBinding
+    private val viewModel: MoviesViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +38,15 @@ class SearchFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.searchMovie.setOnClickListener {
-            findNavController().popBackStack()
+            val searchText = binding.searchTextField.text.toString().trim()
+            if (searchText != ""){
+                Log.d(TAG, "onViewCreated: searchText contains keyword -> $searchText")
+                val keyword = searchText
+                viewModel.searchMovies(keyword)
+                findNavController().popBackStack()
+            }else{
+                binding.searchTextField.error = "Enter the movie to search"
+            }
         }
     }
 }
